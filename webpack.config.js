1b5bcdf522b9690f87bf6ddd695db17841e26fc0
Ilya Rudakov <96202997+RudakovIlya@users.noreply.github.com>
2023-05-11 00:21:01 +0300

    feat: added index.html and new dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,19 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
+  mode: "development",
   /* entry: Entry is the starting point of the application */
   entry: path.resolve(__dirname, 'src', 'index.js'),
   /* output: Where will we put the assembly of our application */
   output: {
-    filename: 'bundle.js',
+    /* "[name].[contenthash].js": Solving the problem of caching files in the browser */
+    filename: '[name].[contenthash].js',
     path: path.resolve(__dirname, 'build'),
+    /* clean: Cleans up redundant files in build */
+    clean: true
   },
+  plugins: [
+    new HtmlWebpackPlugin({template: path.resolve(__dirname, 'public', 'index.html')})
+  ]
 };
