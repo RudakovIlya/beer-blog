ad5f11355b326c8d9604beb4d079780677c4d995
Ilya Rudakov <96202997+RudakovIlya@users.noreply.github.com>
2023-05-13 20:37:03 +0300

    fix: added new plugins

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,7 +1,7 @@
 import webpack from 'webpack';
-import {buildWebpackConfig} from "./config/build/buildWebpackConfig";
-import {BuildEnv, BuildPaths} from "./config/build/types/config";
 import path from "path";
+import {BuildEnv, BuildPaths} from "./config/build/types/config";
+import {buildWebpackConfig} from "./config/build/buildWebpackConfig";
 
 export default (env: BuildEnv): webpack.Configuration => {
   const paths: BuildPaths = {
