89363a600474a84379a2d22ffc0b6b1c0015cc63
Ilya Rudakov <96202997+RudakovIlya@users.noreply.github.com>
2023-05-17 12:10:51 +0300

    feat: remove redundant code

--- src/pages/MainPage/ui/MainPage.tsx
+++ src/pages/MainPage/ui/MainPage.tsx
@@ -2,9 +2,9 @@ import React from 'react'
 import {useTranslation} from "react-i18next";
 
 const MainPage = () => {
-  const {t} = useTranslation()
+  // const {t} = useTranslation()
   return <div>{
-    t('Main Page')
+
   }</div>
 };
 
