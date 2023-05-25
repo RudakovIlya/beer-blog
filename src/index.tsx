430c54c3490accbb925821888461bacc8c697e53
Ilya Rudakov <96202997+RudakovIlya@users.noreply.github.com>
2023-05-25 23:45:23 +0300

    fix: delete comments

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import { render } from 'react-dom'
 import { ThemeProvider } from 'app/providers/ThemeProvider'
 import { RouterProvider } from 'react-router-dom'
 import { router } from 'app/providers/Router'
+
 import 'shared/config/i18n/i18n'
 import './app/styles/index.scss'
 
