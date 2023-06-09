4be618a45038e0369c460e6c428c528c62cc667b
Ilya Rudakov <96202997+RudakovIlya@users.noreply.github.com>
2023-06-09 20:12:49 +0300

    feat: added story

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -13,9 +13,7 @@ export const App = () => {
     <div className={clsx('app', {}, theme)}>
       <Suspense fallback={<PageLoader />}>
         <Navbar />
-
         <div className={'content-page'}>
-
           <Sidebar />
           <ContentProvider />
         </div>
