2c2f500a0220bbfad987870ca410a864ef12736a
Ilya Rudakov <96202997+RudakovIlya@users.noreply.github.com>
2023-06-17 23:58:08 +0300

    feat: added modal component

--- src/shared/config/storybook/RouterDecorator/RouterDecorator.tsx
+++ src/shared/config/storybook/RouterDecorator/RouterDecorator.tsx
@@ -5,7 +5,9 @@ import { BrowserRouter } from 'react-router-dom'
 export const RouterDecorator = (story: () => Story) => {
   return (
     <BrowserRouter>
+
       {story()}
+
     </BrowserRouter>
   )
 }
