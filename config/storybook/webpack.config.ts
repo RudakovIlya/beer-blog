3e42ded735d3e5a1e9c660eb22df590db6e2f4bd
Ilya Rudakov <96202997+RudakovIlya@users.noreply.github.com>
2023-06-18 21:35:10 +0300

    feat: added redux, counter, tests for counter entity

--- config/storybook/webpack.config.ts
+++ config/storybook/webpack.config.ts
@@ -28,5 +28,9 @@ export default ({ config }: { config: webpack.Configuration }) => {
   })
   config.module.rules.push(buildCSSLoaders(true))
 
+  config.plugins?.push(new webpack.DefinePlugin({
+    __IS_DEV__: JSON.stringify(true),
+  }))
+
   return config
 }
--- src/pages/MainPage/ui/MainPage.tsx
+++ src/pages/MainPage/ui/MainPage.tsx
@@ -1,11 +1,9 @@
 import { useTranslation } from 'react-i18next'
-import { Counter } from 'entities/Counter'
 
 const MainPage = () => {
   const { t } = useTranslation()
   return (
     <div>
-      <Counter />
       {t('Hello world')}
     </div>
   )
