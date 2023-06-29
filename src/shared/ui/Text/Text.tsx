9337c95ce6a41b56c95527618523ee0615a8c7f3
Ilya Rudakov <96202997+RudakovIlya@users.noreply.github.com>
2023-06-29 20:24:36 +0300

    feat: added text component

--- src/shared/ui/Text/Text.tsx
+++ src/shared/ui/Text/Text.tsx
@@ -1,14 +1,80 @@
 import React, {
-  ElementType, PropsWithChildren, ComponentProps, memo,
+  ElementType, PropsWithChildren, ComponentProps, memo, CSSProperties,
 } from 'react'
 import { clsx } from 'shared/lib'
 import cls from './Text.module.scss'
 
-type TextAlignProps = 'center' | 'left' | 'right'
+export type TextPropAlign = 'left' | 'center' | 'right';
+
+export type TextPropCursor = 'pointer'
+
+export type TextPropDecoration = 'underline'
+
+export type TextPropLineHeight = '2xs' | 'xs' | 's' | 'm' | 'l';
+
+export type TextPropSize =
+  's' |
+  'xs' |
+  '2xs' |
+  'm' |
+  'l' |
+  'xl' |
+  '2xl' |
+  '3xl' |
+  '4xl' |
+  '5xl' |
+  '6xl'
+
+export type TextPropSpacing = 'xs'| 's'| 'm'| 'l'
+
+export const textPropType = ['blockquote', 'p', 'h3', 'h2', 'h1'] as const
+export type TextPropType = typeof textPropType[number];
+
+export const textPropVariant = [
+  'primary',
+  'secondary',
+  'brand',
+  'ghost',
+  'link',
+  'linkMinor',
+  'system',
+  'normal',
+  'success',
+  'warning',
+  'alert',
+  'caution',
+  'critical',
+] as const
+export type TextPropVariant = typeof textPropVariant[number];
+export const textPropVariantDefault: TextPropVariant = textPropVariant[0]
+
+export const textPropWeight = [
+  'thin',
+  'light',
+  'regular',
+  'medium',
+  'semibold',
+  'bold',
+  'black',
+] as const
+export type TextPropWeight = typeof textPropWeight[number];
+
+export const textPropTransform = ['uppercase'] as const
+export type TextPropTransform = typeof textPropTransform[number];
+
+export const textPropWidth = ['default'] as const
+export type TextPropWidth = typeof textPropWidth[number];
+
+export const textPropDisplay = ['block', 'inlineBlock', 'inline'] as const
+export type TextPropDisplay = typeof textPropDisplay[number];
 
 type TextOwnProps<E extends ElementType = ElementType> = {
-  variant?: string
-  align?: TextAlignProps
+  variant?: TextPropVariant
+  align?: TextPropAlign
+  cursor?: TextPropCursor
+  decoration?: TextPropDecoration
+  display: TextPropDisplay
+  lineHeight?: TextPropLineHeight
   as?: E
 }
 
@@ -18,8 +84,9 @@ const defaultElement = 'p'
 
 export const Text = memo(<E extends ElementType = typeof defaultElement>({
   as,
-  variant,
+  variant = textPropVariantDefault,
   align = 'left',
+  display,
   className,
   children,
   ...otherProps
