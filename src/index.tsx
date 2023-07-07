f5f253f6c8d8265fc2726e695c67e513345d5cec
Ilya Rudakov <96202997+RudakovIlya@users.noreply.github.com>
2023-07-07 23:23:30 +0300

    fix: added new pages Articles and ArticlesDetailsPage, Skeleton

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,9 @@
 import { render } from 'react-dom'
+import { BrowserRouter } from 'react-router-dom'
 import { ThemeProvider } from 'app/providers/ThemeProvider'
 import { StoreProvider } from 'app/providers/StoreProvider'
-import { BrowserRouter } from 'react-router-dom'
-import { App } from 'app/App'
 import { ErrorBoundary } from 'app/providers/ErrorBoundary'
+import { App } from 'app/App'
 import './app/styles/index.scss'
 import './shared/config/i18n/i18n'
 
