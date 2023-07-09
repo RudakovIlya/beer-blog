17f21b5544dc6848d2f04d6f9bf3843481f012d5
Ilya Rudakov <96202997+RudakovIlya@users.noreply.github.com>
2023-07-09 23:22:25 +0300

    feat: added Code component, and test for thunks

--- src/entities/Article/model/services/fetchArticleById/fetchArticleById.test.ts
+++ src/entities/Article/model/services/fetchArticleById/fetchArticleById.test.ts
@@ -17,11 +17,8 @@ const data: Article = {
 describe('fetchArticleById', () => {
   test('should be success', async () => {
     const thunk = new TestAsyncThunk(fetchArticleById)
-
     thunk.api.get.mockReturnValue(Promise.resolve({ data }))
-
     const result = await thunk.callThunk('1')
-
     expect(thunk.api.get).toHaveBeenCalled()
     expect(result.meta.requestStatus).toBe('fulfilled')
     expect(result.payload).toEqual(data)
